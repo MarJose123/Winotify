ae0ce9cbfc72d6ab61fb56b134f1b64214afbb5d
MarJose <18107626+whoami213@users.noreply.github.com>
2018-05-24 08:38:13 +0800

    initial release with example vb.net codes project

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,23 @@
+﻿Imports Winotify.Winotify
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        AlertShow("The Quick Brown Fox jump over the lazy dog!", AlertType.Warning)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        AlertShow("The Quick Brown Fox jump over the lazy dog!", AlertType.Info)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        AlertShow("The Quick Brown Fox jump over the lazy dog!", AlertType.Errors)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        AlertShow("The Quick Brown Fox jump over the lazy dog!", AlertType.Success)
+    End Sub
+End Class
